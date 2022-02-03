0c6a907a6386f67f5ac70040fc833b6f660e02d1
Kanchan Chaudhary <kanchan.chaudhary77@gmail.com>
2022-02-04 01:00:29 +0530

    button styling change

--- src/components/ChangeColor.js
+++ src/components/ChangeColor.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { darkTheme, lightTheme } from '../features/theme';
+import { useSelector } from 'react-redux';
 
 const ChangeColor = () => {
   const dispatch = useDispatch();
+  const theme = useSelector((state) => state.theme.value); //fetching the value
 
   const lightColor = () => {
     dispatch(lightTheme({ background: 'white', text: 'black' }));
@@ -15,8 +17,18 @@ const ChangeColor = () => {
 
   return (
     <div className='theme'>
-      <button onClick={darkColor}>Dark Mode</button>
-      <button onClick={lightColor}>Light Mode</button>
+      <button
+        style={{ color: theme.background, backgroundColor: theme.text }}
+        onClick={darkColor}
+      >
+        Dark Mode
+      </button>
+      <button
+        style={{ color: theme.text, backgroundColor: theme.background }}
+        onClick={lightColor}
+      >
+        Light Mode
+      </button>
     </div>
   );
 };
